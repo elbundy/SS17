29eb344330de46de46391af403cf4656ce55f821
Sebastian Rietsch <sebastian.rietsch@fau.de>
2017-07-02 18:15:48 +0200

    Repo cleanup

--- KI2/ueb6/prob61.py
+++ KI2/ueb6/prob61.py
@@ -18,6 +18,7 @@ def eviEmi(x, e):
     elif(x == 'B' and e == 'J'):
         return 0.3
 
+"""
 prob = 0.0
 x0 = 'H';
 for x1 in ['H', 'B']:
@@ -28,3 +29,27 @@ for x1 in ['H', 'B']:
             prob += probTmp
 
 print(prob)
+"""
+# Calculates P(X_t, e_{1:t}) = P(e_t | X_t) \sum_{x_{t-1}} P(X_t | x_{t-1}) P(x_{t-1} | e_{1:t-1})
+def recursion(t, x, e):
+    #recursion end
+    #return P(X_0), 1 for 'H', 0 for 'B'
+    if t == 0:
+        if x == 'H':
+            return 1
+        else:
+            return 0
+    #P(e_t | X_t)
+    firstProp = eviEmi(x, e[t])
+    sumProp = 0.0
+    for x_minus in ['H', 'B']:
+        #P(X_t | x_{t-1})
+        secondProp = stateTrans(x, x_minus)
+        #P(x_{t-1} | e_{1:t-1})
+        thirdProp = recursion(t-1, x_minus, e)
+        sumProp += secondProp * thirdProp
+    return firstProp * sumProp
+
+e = ['Dummy', 'DM', 'DM', 'DM', 'J']
+x = 'H'
+print(recursion(4, x, e))
